0ef696ad5ecb5c6cfbb712ae6cb17a54d6783318
catmeow123456 <eden@stu.pku.edu.cn>
2025-02-14 12:50:53 +0800

    fix spec

--- src/job/job.service.spec.ts
+++ src/job/job.service.spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { JobService } from './job.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 describe('JobService', () => {
   let service: JobService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [JobService],
+      providers: [JobService, PrismaService],
     }).compile();
 
     service = module.get<JobService>(JobService);
